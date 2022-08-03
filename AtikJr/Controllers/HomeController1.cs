3f5ee4336ea1749b937644d4dd1410c0078df8db
Muhammet Yasir Çolak <muhammetyasircolak@gmail.com>
2022-08-03 15:02:48 +0300

    home controller added. index html designed.

---
+++ AtikJr/Controllers/HomeController1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtikJr.Controllers
+{
+    public class HomeController1 : Controller
+    {
+
+        public string Index()
+        {
+            return "first page";
+        }
+
+        public string Test()
+        {
+            return "test page";
+        }
+
+    }
+}
