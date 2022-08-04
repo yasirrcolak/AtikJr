da3eff6f92b377aa9e26d668a2d9066098f5d7ec
Muhammet Yasir Çolak <muhammetyasircolak@gmail.com>
2022-08-04 13:47:55 +0300

    resim ekleme altyapısı

---
+++ AtikJr/Controllers/atik.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtikJr.Controllers
+{
+    public class atik : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AtikJr/Views/atik/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AtikJr.Views.Home
+{
+    public class atikModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
