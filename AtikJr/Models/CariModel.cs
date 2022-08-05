f1d80ac41d357f9a586bafcb2a99da7b2ad0c29b
Muhammet Yasir Çolak <muhammetyasircolak@gmail.com>
2022-08-05 09:34:07 +0300

    modeller eklendi. proje view ve controller eklendi. modeller için şablon verildi.

---
+++ AtikJr/Models/CariModel.cs
@@ -0,0 +1,23 @@
+﻿namespace AtikJr.Models
+{
+    public class CariModel
+    {
+        /// <summary>
+        /// id si
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// ismi
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Oluşturulma tarihi.
+        /// </summary>
+        public string Date { get; set; }
+
+
+        // vs vs özellikler
+    }
+}
