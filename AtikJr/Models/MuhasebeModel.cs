00bb297b70a7617f77f40bc442f2a07169e4c664
Ahmetaydogduu <aydogduahmetsamet@gmail.com>
2022-08-08 11:57:36 +0300

    muhasebe modeli düzenlendi

--- AtikJr/Models/MuhasebeModel.cs
+++ AtikJr/Models/MuhasebeModel.cs
@@ -2,5 +2,47 @@
 {
     public class MuhasebeModel
     {
+        /// <summary>
+        /// muhasebe işlem sırası
+       /// </summary>
+        public int MUHASEBE_ID { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summhesap ismiary>
+        public string MUHASEBE_HESAP_ADI { get; set; }
+
+        /// <summary>
+        /// hesap türü açıklama (bireysel, kurumsal vb)
+        /// </summary>
+        public string MUHASEBE_HESAP_TURU { get; set; }
+
+        /// <summary>
+        /// borç miktarı
+        /// </summary>
+        public float MUHASEBE_TOPLAM_BORC { get; set; }
+
+        /// <summary>
+        /// muhasebe işlem tarihi
+        /// </summary>
+        public DateTime MUHASEBE_TARIH { get; set; }
+
+        /// <summary>
+        /// kontrol etmek için kullanılan kod
+        /// </summary>
+        public int MUHASEBE_KONTROL_KODU { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
