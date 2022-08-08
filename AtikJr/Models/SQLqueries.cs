46274708c45b1880ed23f27eaca65d9b4e630fbb
Ahmetaydogduu <aydogduahmetsamet@gmail.com>
2022-08-08 14:50:07 +0300

    SELECT KOMUTLARI GÜNCELLENDİ

--- AtikJr/Models/SQLqueries.cs
+++ AtikJr/Models/SQLqueries.cs
@@ -15,12 +15,12 @@
         /// <summary>
         /// cari tablosundan tüm veriler çekildi.
         /// </summary>
-        public static string tumCariler = " select * from TBL_CARI ";
+        public static string tumCariler = "SELECT * from TBL_CARI";
 
         /// <summary>
         /// kasa tablosundan tüm veriler çekildi.
         /// </summary>
-        public static string tumKasalar = " select * from TBL_KASA ";
+        public static string tumKasalar = "SELECT * FROM TBL_KASA";
 
         /// <summary>
         /// stok tablosundan tüm veriler çekildi.
@@ -30,29 +30,38 @@
         /// <summary>
         /// muhasebe tablosundan tüm veriler çekildi.
         /// </summary>
-        public static string tumMuhasebeler = " select * from TBL_MUHASEBE ";
+        public static string tumMuhasebeler = "SELECT * FROM TBL_MUHASEBE";
 
         /// <summary>
         /// banka tablosundan tüm veriler çekildi.
         /// </summary>
-        public static string tumBankalar = " select * from TBL_BANKA ";
+        public static string tumBankalar = "SELECT * FROM TBL_BANKA";
 
         /// <summary>
         /// proje tablosundan tüm veriler çekildi.
         /// </summary>
-        public static string tumProjeler = " select * from TBL_PROJE ";
+        public static string tumProjeler = "SELECT * FROM TBL_PROJE";
 
         /// <summary>
         /// araç tablosundan tüm veriler çekildi.
         /// </summary>
-        public static string tumAraclar = " select * from TBL_ARACC ";
+        public static string tumAraclar = "SELECT * FROM TBL_ARACC";
 
         /// <summary>
         /// besi tablosundan tüm veriler çekildi.
         /// </summary>
-        public static string tumBesiler = " select * from TBL_BESI ";
+        public static string tumBesiler = "SELECT * FROM TBL_BESI ";
 
-        #endregion 
+        #endregion
+
+        #region Delete Sorguları
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string CariSil = "DELETE tablo adı where ID=";
+
+        #endregion
 
     }
 }
