7931c719ae64ca6d70a083ddbcda0b8c8baaa1d6
Muhammet Yasir Çolak <muhammetyasircolak@gmail.com>
2022-08-08 11:27:33 +0300

    cari controller cari getir metodu eklendi

--- AtikJr/Controllers/CariController.cs
+++ AtikJr/Controllers/CariController.cs
@@ -1,4 +1,5 @@
-﻿using AtikJr.Models;
+﻿using System.Data.SqlClient;
+using AtikJr.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AtikJr.Controllers
@@ -7,10 +8,9 @@ namespace AtikJr.Controllers
     {
         public IActionResult Index()
         {
+            CariGetir();
             return View();
         }
-
-
 
         /// <summary>
         /// Veritabanındaki tüm kayıtlı verileri içinde barındıran nesneleri tutan liste.
@@ -22,7 +22,40 @@ namespace AtikJr.Controllers
         /// </summary>
         public void CariGetir()
         {
+            try
+            {
+                String connectionString = "connection string......";
 
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sqlQuery = "Select asdasdasds.......";
+
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                CariModel cari = new CariModel();
+
+                                cari.CARI_ID = reader.GetInt32(0);
+                                cari.CARI_TARIH = reader.GetDateTime(1);
+                                cari.CARI_BORC = reader.GetInt32(2);
+                                cari.CARI_KOD = reader.GetInt32(3);
+                                cari.CARI_ADRES = reader.GetString(4);
+                                cari.CARI_TELEFON = reader.GetString(5);
+
+                                cariListesi.Add(cari);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata!!! : " + ex.Message.ToString());
+            }
         }
 
 
