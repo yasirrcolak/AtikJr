741db01cdd9a1012921e874c526ebd5ca759418d
sertaç çay <sertac_3820@hotmail.com>
2022-08-08 11:56:42 +0300

    Proje Model düzenlendi

--- AtikJr/Models/ProjeModel.cs
+++ AtikJr/Models/ProjeModel.cs
@@ -2,5 +2,36 @@
 {
     public class ProjeModel
     {
+        /// <summary>
+        /// Projenin sıra numarası
+        /// </summary>
+        public int PROJE_ID { get; set; }
+
+        /// <summary>
+        /// Projenin adı
+        /// </summary>
+        public string PROJE_ADI { get; set; }
+
+        /// <summary>
+        /// Projeyi yöneten kişi
+        /// </summary>
+        public string PROJE_SORUMLUSU { get; set; }
+
+        /// <summary>
+        /// Projede çalışan sayısı
+        /// </summary>
+        public int PROJE_CALISAN_SAYISI { get; set; }
+
+        /// <summary>
+        /// Projenin teslim tarihi
+        /// </summary>
+        public DateTime PROJE_TESLIM_TARIHI { get; set; }
+
+        /// <summary>
+        /// Proje maliyeti
+        /// </summary>
+        public float PROJE_MALIYET { get; set; }
+
+
     }
 }
